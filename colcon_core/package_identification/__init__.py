fbd103812b6cfee443519c4eddd1c3d724603859
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2018-02-15 20:32:19 -0800

    add package identification extension point

---
+++ colcon_core/package_identification/__init__.py
@@ -0,0 +1,169 @@
+# Copyright 2016-2018 Dirk Thomas
+# Licensed under the Apache License, Version 2.0
+
+import copy
+import traceback
+from typing import Dict
+from typing import TypeVar
+
+from colcon_core.logging import colcon_logger
+from colcon_core.package_descriptor import PackageDescriptor
+from colcon_core.plugin_system import instantiate_extensions
+from colcon_core.plugin_system import order_extensions_grouped_by_priority
+
+logger = colcon_logger.getChild(__name__)
+
+
+class SkipLocationException(Exception):
+    """
+    Exception to signal that a path should be skipped.
+
+    This also excludes all recursive subdirectories from being considered.
+
+    It should be raised in the
+    :function:`PackageIdentificationExtensionPoint.identify` method of package
+    identification extensions.
+    """
+
+    pass
+
+
+class PackageIdentificationExtensionPoint:
+    """
+    The interface for package identification extensions.
+
+    A package identification extension populates a package descriptor if a
+    given location contains a package.
+
+    For each instance the attribute `PACKAGE_IDENTIFICATION_NAME` is being set
+    to the basename of the entry point registering the extension.
+    """
+
+    """The version of the package identification extension interface."""
+    EXTENSION_POINT_VERSION = '1.0'
+
+    """The default priority of package identification extensions."""
+    PRIORITY = 100
+
+    def identify(self, desc: PackageDescriptor):
+        """
+        Check if the given path contains a package.
+
+        If yes, update the package descriptor with additional information.
+        If the descriptor already has a package type only act if the type
+        matches this extension.
+
+        This method must be overridden in a subclass.
+
+        :param desc: The package descriptor with the directory to check
+        :type desc:
+          :py:class:`colcon_core.package_descriptor.PackageDescriptor`
+        :raises SkipLocationException: Skip the path as well as all recursive
+          subdirectories
+        """
+        raise NotImplementedError()
+
+
+def get_package_identification_extensions():
+    """
+    Get the available package identification extensions.
+
+    The extensions are grouped by their priority and each group is ordered by
+    the entry point name.
+
+    :rtype: OrderedDict
+    """
+    extensions = instantiate_extensions(__name__)
+    for name, extension in extensions.items():
+        extension.PACKAGE_IDENTIFICATION_NAME = name
+    return order_extensions_grouped_by_priority(extensions)
+
+
+ReturnType = TypeVar('ReturnType', None, bool, PackageDescriptor)
+
+
+def identify(
+    extensions: Dict[  # actually an OrderedDict
+        int,  # priority
+        Dict[str, PackageIdentificationExtensionPoint],  # an OrderedDict
+    ],
+    path: str,
+) -> ReturnType:
+    """
+    Identify the package in the given path.
+
+    :param extensions: dict of extensions
+    :param path: The path
+    """
+    desc = PackageDescriptor(path)
+
+    for extensions_same_prio in extensions.values():
+        result = _identify(extensions_same_prio, desc)
+
+        # continue with next priority level if no information was contributed
+        if result is None:
+            continue
+
+        # skip location since identification is ambiguous
+        if result is False:
+            raise SkipLocationException()
+
+        assert isinstance(result, PackageDescriptor), result
+        if result.identifies_package():
+            return result
+
+        # use incrementally populated descriptor for next priority level
+        desc = result
+
+    if getattr(desc, 'type', None) or getattr(desc, 'name', None):
+        logger.warn(
+            "package '{desc.path}' has type or name but is incomplete"
+            .format_map(locals()))
+    return None
+
+
+def _identify(extensions_same_prio, desc):
+    logger.log(
+        1, '_identify(%s) by extensions %s',
+        desc.path, sorted(extensions_same_prio.keys()))
+    # collect the optionally modified descriptors from all extensions
+    results = {desc}
+    for key, extension in extensions_same_prio.items():
+        desc2 = copy.deepcopy(desc)
+        logger.log(1, "_identify(%s) by extension '%s'", desc.path, key)
+        try:
+            retval = extension.identify(desc2)
+            assert retval is None, 'identify() should return None'
+        except SkipLocationException:
+            logger.log(1, '_identify(%s) ignored', desc.path)
+            raise
+        except Exception as e:
+            # catch exceptions raised in identification extension
+            exc = traceback.format_exc()
+            logger.error(
+                'Exception in package identification extension '
+                "'{extension.PACKAGE_IDENTIFICATION_NAME}' in '{desc.path}': "
+                '{e}\n{exc}'.format_map(locals()))
+            # skip failing extension, continue with next one
+            continue
+
+        results.add(desc2)
+
+    # multiple extensions populated the descriptor with different values
+    if len(results) > 2:
+        logger.warn(
+            '_identify({desc.path}) has multiple matches and therefore is '
+            'being ignored: '.format_map(locals()) +
+            ', '.join(sorted(d.type for d in results if d.type is not None)))
+        return False
+
+    # no extension populated the descriptor with any values
+    if len(results) == 1:
+        return None
+
+    results.remove(desc)
+    desc = results.pop()
+    logger.debug(
+        "Package '{desc.path}' with type '{desc.type}' and name '{desc.name}'"
+        .format_map(locals()))
+    return desc
---
+++ test/test_package_identification.py
@@ -0,0 +1,187 @@
+# Copyright 2016-2018 Dirk Thomas
+# Licensed under the Apache License, Version 2.0
+
+import os
+
+from colcon_core.package_descriptor import PackageDescriptor
+from colcon_core.package_identification import _identify
+from colcon_core.package_identification \
+    import get_package_identification_extensions
+from colcon_core.package_identification import identify
+from colcon_core.package_identification \
+    import PackageIdentificationExtensionPoint
+from colcon_core.package_identification import SkipLocationException
+from mock import Mock
+from mock import patch
+import pytest
+
+from .entry_point_context import EntryPointContext
+
+
+class Extension1(PackageIdentificationExtensionPoint):
+    PRIORITY = 80
+
+
+class Extension2(PackageIdentificationExtensionPoint):
+    pass
+
+
+class Extension3(PackageIdentificationExtensionPoint):
+    PRIORITY = 90
+
+
+class Extension4(PackageIdentificationExtensionPoint):
+    pass
+
+
+def test_get_package_identification_extensions():
+    with EntryPointContext(
+        extension1=Extension1, extension2=Extension2,
+        extension3=Extension3, extension4=Extension4,
+    ):
+        extensions = get_package_identification_extensions()
+    assert list(extensions.keys()) == [100, 90, 80]
+    assert list(extensions[100].keys()) == ['extension2', 'extension4']
+    assert list(extensions[90].keys()) == ['extension3']
+    assert list(extensions[80].keys()) == ['extension1']
+
+
+def identify_name(desc):
+    desc.name = 'name'
+
+
+def identify_type(desc):
+    desc.type = 'type'
+
+
+def identify_name_and_type(desc):
+    identify_name(desc)
+    identify_type(desc)
+
+
+def test_identify():
+    path = '/some/path'
+    context = EntryPointContext(
+        extension1=Extension1, extension2=Extension2,
+        extension3=Extension3, extension4=Extension4)
+
+    with context:
+        # no identification
+        desc = identify({}, path)
+        assert desc is None
+
+        # no complete identification
+        extensions = get_package_identification_extensions()
+        extensions[80]['extension1'].identify = Mock(
+            side_effect=identify_name)
+        desc = identify(extensions, path)
+        assert desc is None
+
+        # valid result combined across priority groups
+        extensions = get_package_identification_extensions()
+        extensions[100]['extension4'].identify = Mock(
+            side_effect=identify_type)
+        desc = identify(extensions, path)
+        assert isinstance(desc, PackageDescriptor)
+        assert str(desc.path) == '/some/path'.replace('/', os.sep)
+        assert desc.name == 'name'
+        assert desc.type == 'type'
+
+        # skip location
+        extensions = get_package_identification_extensions()
+        extensions[90]['extension3'].identify = Mock(
+            side_effect=SkipLocationException())
+        with pytest.raises(SkipLocationException):
+            identify(extensions, path)
+
+        # valid result from first priority group
+        # lower priority groups are not even invoked
+        extensions = get_package_identification_extensions()
+        extensions[100]['extension4'].identify.side_effect = \
+            identify_name_and_type
+        desc = identify(extensions, path)
+        assert isinstance(desc, PackageDescriptor)
+        assert str(desc.path) == '/some/path'.replace('/', os.sep)
+        assert desc.name == 'name'
+        assert desc.type == 'type'
+
+    with context:
+        # multiple different results result in skipping the location
+        extensions = get_package_identification_extensions()
+        extensions[100]['extension2'].identify = Mock(
+            side_effect=identify_name)
+        extensions[100]['extension4'].identify = Mock(
+            side_effect=identify_type)
+        with pytest.raises(SkipLocationException):
+            identify(extensions, path)
+
+
+def test__identify():
+    desc_path_only = PackageDescriptor('/some/path')
+    with EntryPointContext(
+        extension1=Extension1, extension2=Extension2,
+        extension3=Extension3, extension4=Extension4,
+    ):
+        # valid result
+        extensions = get_package_identification_extensions()[100]
+        extensions['extension2'].identify = Mock()
+        extensions['extension4'].identify = identify_name_and_type
+        desc = _identify(extensions, desc_path_only)
+        assert isinstance(desc, PackageDescriptor)
+        assert str(desc.path) == '/some/path'.replace('/', os.sep)
+        assert desc.name == 'name'
+        assert desc.type == 'type'
+
+        # no results
+        extensions = get_package_identification_extensions()[100]
+        extensions['extension2'].identify = Mock()
+        extensions['extension4'].identify = Mock()
+        desc = _identify(extensions, desc_path_only)
+        assert desc is None
+
+        # multiple different results
+        extensions = get_package_identification_extensions()[100]
+        extensions['extension2'].identify = identify_name
+        extensions['extension4'].identify = identify_type
+        with patch('colcon_core.package_identification.logger.warn') as warn:
+            desc = _identify(extensions, desc_path_only)
+            assert desc is False
+            # the raised exception is catched and results in a warn message
+            assert warn.call_count == 1
+            assert len(warn.call_args[0]) == 1
+            assert 'multiple matches' in warn.call_args[0][0]
+
+        # invalid return value
+        extensions = get_package_identification_extensions()[90]
+        extensions['extension3'].identify = Mock(return_value=True)
+        with patch('colcon_core.package_identification.logger.error') as error:
+            desc = _identify(extensions, desc_path_only)
+            assert desc is None
+            # the raised exception is catched and results in an error message
+            assert error.call_count == 1
+            assert len(error.call_args[0]) == 1
+            assert error.call_args[0][0].startswith(
+                "Exception in package identification extension 'extension3' "
+                "in '/some/path': identify() should return None\n"
+                .replace('/', os.sep))
+
+        # skip location
+        extensions = get_package_identification_extensions()[90]
+        extensions['extension3'].identify = Mock(
+            side_effect=SkipLocationException())
+        with pytest.raises(SkipLocationException):
+            _identify(extensions, desc_path_only)
+
+        # raise exception
+        extensions = get_package_identification_extensions()[90]
+        extensions['extension3'].identify = Mock(
+            side_effect=RuntimeError('custom exception'))
+        with patch('colcon_core.package_identification.logger.error') as error:
+            desc = _identify(extensions, desc_path_only)
+            assert desc is None
+            # the raised exception is catched and results in an error message
+            assert error.call_count == 1
+            assert len(error.call_args[0]) == 1
+            assert error.call_args[0][0].startswith(
+                "Exception in package identification extension 'extension3' "
+                "in '/some/path': custom exception\n".replace('/', os.sep))
