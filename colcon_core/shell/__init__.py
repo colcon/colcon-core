c2c6421e0d84ff9db3f127073c77804bb30437b6
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2018-02-21 19:03:50 -0800

    add shell extension point

---
+++ colcon_core/shell/__init__.py
@@ -0,0 +1,306 @@
+# Copyright 2016-2018 Dirk Thomas
+# Licensed under the Apache License, Version 2.0
+
+from concurrent.futures import CancelledError
+import os
+from pathlib import Path
+import traceback
+
+from colcon_core.environment_variable import EnvironmentVariable
+from colcon_core.logging import colcon_logger
+from colcon_core.plugin_system import instantiate_extensions
+from colcon_core.plugin_system import order_extensions_grouped_by_priority
+from colcon_core.plugin_system import SkipExtensionException
+from colcon_core.subprocess import check_output
+
+logger = colcon_logger.getChild(__name__)
+
+"""Environment variable to enable all shell extensions."""
+ALL_SHELLS_ENVIRONMENT_VARIABLE = EnvironmentVariable(
+    'COLCON_ALL_SHELLS', 'Flag to enable all shell extensions')
+
+use_all_shell_extensions = os.environ.get(
+    ALL_SHELLS_ENVIRONMENT_VARIABLE.name, False)
+
+
+class ShellExtensionPoint:
+    """
+    The interface for shell extensions.
+
+    An shell extension generates the scripts for a specific shell to setup the
+    environment.
+
+    For each instance the attribute `SHELL_NAME` is being set to the basename
+    of the entry point registering the extension.
+    """
+
+    """The version of the shell extension interface."""
+    EXTENSION_POINT_VERSION = '1.0'
+
+    """
+    The default priority of shell extensions.
+
+    A shell extension must use a higher priority than the default if and only
+    if it is a "primary" shell.
+    A "primary" shell does not depend on another shell to setup the
+    environment, e.g. `sh`.
+    An example for a "non-primary" shell would be `bash` which relies on the
+    `sh` shell extension to setup environment variables and only contributes
+    additional information like completion.
+
+    All "non-primiry" shell extensions must use a priority equal to or lower
+    than the default.
+    """
+    PRIORITY = 100
+
+    def create_prefix_script(self, prefix_path, pkg_names, merge_install):
+        """
+        Create a script in the install prefix path.
+
+        The script should call each package specific script in order.
+
+        This method must be overridden in a subclass.
+
+        :param Path prefix_path: The path of the install prefix
+        :param list pkg_names: The package names
+        :param bool merge_install: The flag if all packages share the same
+          install prefix
+        """
+        raise NotImplementedError()
+
+    def create_package_script(self, prefix_path, pkg_name, hooks):
+        """
+        Create a script for a specific package.
+
+        The script should call each hook script in order.
+
+        This method must be overridden in a subclass.
+
+        :param Path prefix_path: The package specific install prefix
+        :param str pkg_name: The package name
+        :param list hooks: The relative paths to the hook scripts
+        """
+        raise NotImplementedError()
+
+    def create_hook_set_value(
+        self, env_hook_name, prefix_path, pkg_name, name, value,
+    ):
+        """
+        Create a hook script to set an environment variable value.
+
+        This method must be overridden in a subclass.
+
+        :param str env_hook_name: The name of the hook script
+        :param Path prefix_path: The path of the install prefix
+        :param str pkg_name: The package name
+        :param str name: The name of the environment variable
+        :param str value: The value to be set
+        :returns: The relative path to the created hook script
+        :rtype: Path
+        """
+        raise NotImplementedError()
+
+    def create_hook_append_value(
+        self, env_hook_name, prefix_path, pkg_name, name, value,
+    ):
+        """
+        Create a hook script to append a value to an environment variable.
+
+        This method must be overridden in a subclass.
+
+        :param str env_hook_name: The name of the hook script
+        :param Path prefix_path: The path of the install prefix
+        :param str pkg_name: The package name
+        :param str name: The name of the environment variable
+        :param str value: The value to be appended
+        :returns: The relative path to the created hook script
+        :rtype: Path
+        """
+        raise NotImplementedError()
+
+    def create_hook_prepend_value(
+        self, env_hook_name, prefix_path, pkg_name, name, subdirectory,
+    ):
+        """
+        Create a hook script to prepend a value to an environment variable.
+
+        This method must be overridden in a subclass.
+
+        :param str env_hook_name: The name of the hook script
+        :param Path prefix_path: The path of the install prefix
+        :param str pkg_name: The package name
+        :param str name: The name of the environment variable
+        :param str subdirectory: The subdirectory of the prefix path
+        :returns: The relative path to the created hook script
+        :rtype: Path
+        """
+        raise NotImplementedError()
+
+    def create_hook_include_file(
+        self, env_hook_name, prefix_path, pkg_name, relative_path,
+    ):
+        """
+        Create a hook script to include another script.
+
+        This method must be overridden in a subclass.
+
+        :param str env_hook_name: The name of the hook script
+        :param Path prefix_path: The path of the install prefix
+        :param str pkg_name: The package name
+        :param str relative_path: The path of the included scripts
+        :returns: The relative path to the created hook script
+        :rtype: Path
+        """
+        raise NotImplementedError()
+
+    async def generate_command_environment(
+        self, task_name, build_base, dependencies,
+    ):
+        """
+        Get the environment variables to invoke commands.
+
+        The method must be overridden in a subclass if and only if the shell
+        extension represents a "primary" shell (as defined in
+        :attribute:`ShellExtensionPoint.PRIORITY`).
+
+        :param str task_name: The name of the task
+        :param Path build_base: The base path of the build directory
+        :param set dependencies: The name of the recursive dependencies
+        :returns: The environment
+        :rtype: dict
+        :raises SkipExtensionException: if the shell is not usable on the
+          current platform
+        """
+        raise NotImplementedError()
+
+
+def get_shell_extensions():
+    """
+    Get the available shell extensions.
+
+    The extensions are grouped by their priority and each group is ordered by
+    the entry point name.
+
+    :rtype: OrderedDict
+    """
+    extensions = instantiate_extensions(__name__)
+    for name, extension in extensions.items():
+        extension.SHELL_NAME = name
+    return order_extensions_grouped_by_priority(extensions)
+
+
+async def get_command_environment(task_name, build_base, dependencies):
+    """
+    Get the environment variables to invoke commands.
+
+    :param str task_name: The task name identifying a group of task extensions
+    :param str build_base: The path of the build base
+    :param dependencies: The ordered dictionary mapping dependency names to
+      their paths
+    """
+    extensions = get_shell_extensions()
+    for priority in extensions.keys():
+        extensions_same_prio = extensions[priority]
+        for extension in extensions_same_prio.values():
+            try:
+                # use the environment of the first successful shell extension
+                return await extension.generate_command_environment(
+                    task_name, Path(build_base), dependencies)
+            except NotImplementedError:
+                # skip extension, continue with next one
+                logger.debug(
+                    "Skip shell extension '{extension.SHELL_NAME}' for "
+                    'command environment'.format_map(locals()))
+            except SkipExtensionException as e:
+                # skip extension, continue with next one
+                logger.info(
+                    "Skip shell extension '{extension.SHELL_NAME}' for "
+                    'command environment: {e}'.format_map(locals()))
+            except (CancelledError, RuntimeError):
+                # re-raise same exception to handle it in the executor
+                # without a traceback
+                raise
+            except Exception as e:
+                # catch exceptions raised in shell extension
+                exc = traceback.format_exc()
+                logger.error(
+                    'Exception in shell extension '
+                    "'{extension.SHELL_NAME}': {e}\n{exc}"
+                    .format_map(locals()))
+                # skip failing extension, continue with next one
+    raise RuntimeError(
+        'Could not find a shell extension for the command environment')
+
+
+async def get_environment_variables(cmd, cwd=None):
+    """
+    Get the environment variables from the output of the command.
+
+    :param args: the sequence of program arguments
+    :param cwd: the working directory for the subprocess
+    :param shell: whether to use the shell as the program to execute
+    :rtype: dict
+    """
+    output = await check_output(cmd, cwd=cwd, shell=True)
+    env = {}
+    for line in output.splitlines():
+        line = line.rstrip()
+        key, value = line.decode().split('=', 1)
+        env[key] = value
+    return env
+
+
+def create_environment_hook(
+    env_hook_name, prefix_path, pkg_name, name, subdirectory, *, mode='prepend'
+):
+    """
+    Create a hook script for each primary shell.
+
+    :param str env_hook_name: The name of the hook script
+    :param Path prefix_path: The path of the install prefix
+    :param str pkg_name: The package name
+    :param str name: The name of the environment variable
+    :param str subdirectory: The value to be appended
+    :param str mode: The mode how the new value should be combined with an
+      existing value, currently only the value `prepend` is supported
+    :returns: The relative paths to the created hook scripts
+    :rtype: list
+    """
+    logger.log(
+        1, "create_environment_hook('%s', '%s')" % (pkg_name, env_hook_name))
+
+    hooks = []
+    extensions = get_shell_extensions()
+    for priority in extensions.keys():
+        # only consider primary shell extensions
+        if priority <= ShellExtensionPoint.PRIORITY:
+            break
+
+        extensions_same_prio = extensions[priority]
+        for extension in extensions_same_prio.values():
+            if mode == 'prepend':
+                try:
+                    hook = extension.create_hook_prepend_value(
+                        env_hook_name, prefix_path, pkg_name, name,
+                        subdirectory)
+                    assert isinstance(hook, Path), \
+                        'create_hook_prepend_value() should return a Path ' \
+                        'object'
+
+                except Exception as e:
+                    # catch exceptions raised in shell extension
+                    exc = traceback.format_exc()
+                    logger.error(
+                        'Exception in shell extension '
+                        "'{extension.SHELL_NAME}': {e}\n{exc}"
+                        .format_map(locals()))
+                    # skip failing extension, continue with next one
+                    continue
+                hooks.append(hook)
+            else:
+                raise NotImplementedError()
+    if not hooks:
+        raise RuntimeError(
+            'Could not find a primary shell extension for creating an '
+            'environment hook')
+    return hooks
---
+++ test/test_shell.py
@@ -0,0 +1,174 @@
+# Copyright 2016-2018 Dirk Thomas
+# Licensed under the Apache License, Version 2.0
+
+import os
+from pathlib import Path
+
+from colcon_core.plugin_system import SkipExtensionException
+from colcon_core.shell import create_environment_hook
+from colcon_core.shell import get_command_environment
+from colcon_core.shell import get_environment_variables
+from colcon_core.shell import get_shell_extensions
+from colcon_core.shell import ShellExtensionPoint
+from mock import Mock
+from mock import patch
+import pytest
+
+from .entry_point_context import EntryPointContext
+from .run_until_complete import run_until_complete
+
+
+class Extension1(ShellExtensionPoint):
+    PRIORITY = 90
+
+
+class Extension2(ShellExtensionPoint):
+    pass
+
+
+def test_extension_interface():
+    extension = Extension1()
+    with pytest.raises(NotImplementedError):
+        extension.create_prefix_script(None, None, None)
+    with pytest.raises(NotImplementedError):
+        extension.create_package_script(None, None, None)
+    with pytest.raises(NotImplementedError):
+        extension.create_hook_set_value(None, None, None, None, None)
+    with pytest.raises(NotImplementedError):
+        extension.create_hook_append_value(None, None, None, None, None)
+    with pytest.raises(NotImplementedError):
+        extension.create_hook_prepend_value(None, None, None, None, None)
+    with pytest.raises(NotImplementedError):
+        extension.create_hook_include_file(None, None, None, None)
+
+    coroutine = extension.generate_command_environment(None, None, None)
+    with pytest.raises(NotImplementedError):
+        run_until_complete(coroutine)
+
+
+def test_get_shell_extensions():
+    with EntryPointContext(extension1=Extension1, extension2=Extension2):
+        extensions = get_shell_extensions()
+    assert list(extensions.keys()) == [100, 90]
+    assert list(extensions[100].keys()) == ['extension2']
+    assert list(extensions[90].keys()) == ['extension1']
+
+
+async def generate_command_environment(task_name, build_base, dependencies):
+    return {'key': 'value'}
+
+
+def test_get_command_environment():
+    with EntryPointContext(extension1=Extension1, extension2=Extension2):
+        extensions = get_shell_extensions()
+
+        # one not implemented, one skipped extension
+        extensions[90]['extension1'].generate_command_environment = Mock(
+            side_effect=SkipExtensionException())
+        coroutine = get_command_environment(None, '/build/base', None)
+        with patch('colcon_core.shell.logger.debug') as debug:
+            with patch('colcon_core.shell.logger.info') as info:
+                with pytest.raises(RuntimeError) as e:
+                    run_until_complete(coroutine)
+        assert 'Could not find a shell extension for the command environment' \
+            in str(e)
+        assert extensions[90]['extension1'].generate_command_environment \
+            .call_count == 1
+        # the raised exceptions are catched and result in a debug/info message
+        assert debug.call_count == 1
+        assert len(debug.call_args[0]) == 1
+        assert debug.call_args[0][0] == \
+            "Skip shell extension 'extension2' for command environment"
+        assert info.call_count == 1
+        assert len(info.call_args[0]) == 1
+        assert info.call_args[0][0].startswith(
+            "Skip shell extension 'extension1' for command environment: ")
+
+        # raise runtime error
+        extensions[100]['extension2'].generate_command_environment = Mock(
+            side_effect=RuntimeError('custom exception'))
+        extensions[90]['extension1'].generate_command_environment.reset_mock()
+        coroutine = get_command_environment(None, '/build/base', None)
+        with pytest.raises(RuntimeError) as e:
+            run_until_complete(coroutine)
+        assert str(e).endswith(': custom exception')
+        assert extensions[90]['extension1'].generate_command_environment \
+            .call_count == 0
+
+        # one exception, one successful
+        extensions[100]['extension2'].generate_command_environment = Mock(
+            side_effect=Exception('custom exception'))
+        extensions[90]['extension1'].generate_command_environment = Mock(
+            side_effect=generate_command_environment)
+        coroutine = get_command_environment(None, '/build/base', None)
+        with patch('colcon_core.shell.logger.error') as error:
+            env = run_until_complete(coroutine)
+        assert env == {'key': 'value'}
+        # the raised exception is catched and results in an error message
+        assert error.call_count == 1
+        assert len(error.call_args[0]) == 1
+        assert error.call_args[0][0].startswith(
+            "Exception in shell extension 'extension2': custom exception\n")
+
+
+def test_get_environment_variables():
+    cmd = ['echo', 'NAME=value', '&&', 'echo', 'NAME2=value with spaces']
+
+    coroutine = get_environment_variables(cmd)
+    env = run_until_complete(coroutine)
+
+    assert len(env.keys()) == 2
+    assert 'NAME' in env.keys()
+    assert env['NAME'] == 'value'
+    assert 'NAME2' in env.keys()
+    assert env['NAME2'] == 'value with spaces'
+
+
+class Extension3(ShellExtensionPoint):
+    PRIORITY = 105
+
+
+class Extension4(ShellExtensionPoint):
+    PRIORITY = 101
+
+
+class Extension5(ShellExtensionPoint):
+    PRIORITY = 110
+
+
+def test_create_environment_hook():
+    with EntryPointContext(extension1=Extension1, extension2=Extension2):
+        # no primary shell extension
+        with pytest.raises(RuntimeError) as e:
+            create_environment_hook(None, None, None, None, None)
+        assert str(e).endswith(
+            'Could not find a primary shell extension for creating an '
+            'environment hook')
+
+    with EntryPointContext(
+        extension3=Extension3, extension4=Extension4, extension5=Extension5
+    ):
+        extensions = get_shell_extensions()
+
+        # one invalid, two valid return values
+        extensions[105]['extension3'].create_hook_prepend_value = Mock()
+        extensions[101]['extension4'].create_hook_prepend_value = Mock(
+            return_value=Path('/some/path/sub/hookA'))
+        extensions[110]['extension5'].create_hook_prepend_value = Mock(
+            return_value=Path('/some/path/sub/hookB'))
+        with patch('colcon_core.shell.logger.error') as error:
+            hooks = create_environment_hook(None, None, None, None, None)
+        assert len(hooks) == 2
+        assert str(hooks[0]) == '/some/path/sub/hookB'.replace('/', os.sep)
+        assert str(hooks[1]) == '/some/path/sub/hookA'.replace('/', os.sep)
+        # the raised exception is catched and results in an error message
+        assert error.call_count == 1
+        assert len(error.call_args[0]) == 1
+        assert error.call_args[0][0].startswith(
+            "Exception in shell extension 'extension3': "
+            'create_hook_prepend_value() should return a Path object')
+
+        # invalid mode
+        with pytest.raises(NotImplementedError):
+            create_environment_hook(
+                None, None, None, None, None, mode='invalid')
