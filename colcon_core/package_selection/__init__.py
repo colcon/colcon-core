02b7dd59895618ea7162c4645a90b3d586529ba8
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2018-02-21 19:34:21 -0800

    add package selection extension point

---
+++ colcon_core/package_selection/__init__.py
@@ -0,0 +1,224 @@
+# Copyright 2016-2018 Dirk Thomas
+# Licensed under the Apache License, Version 2.0
+
+import traceback
+
+from colcon_core.logging import colcon_logger
+from colcon_core.package_augmentation import augment_packages
+from colcon_core.package_discovery import add_package_discovery_arguments
+from colcon_core.package_discovery import discover_packages
+from colcon_core.package_identification \
+    import get_package_identification_extensions
+from colcon_core.plugin_system import instantiate_extensions
+from colcon_core.plugin_system import order_extensions_by_name
+from colcon_core.topological_order import topological_order_packages
+
+logger = colcon_logger.getChild(__name__)
+
+
+class PackageSelectionExtensionPoint:
+    """
+    The interface for package selection extensions.
+
+    A package selection extension determines the subset of packages to be
+    processed.
+
+    For each instance the attribute `PACKAGE_SELECTION_NAME` is being set to
+    the basename of the entry point registering the extension.
+    """
+
+    """The version of the package selection extension interface."""
+    EXTENSION_POINT_VERSION = '1.0'
+
+    def add_arguments(self, *, parser):
+        """
+        Add command line arguments specific to the task.
+
+        The method is intended to be overridden in a subclass.
+
+        :param parser: The argument parser
+        """
+        pass
+
+    def check_parameters(self, *, args, pkg_names):
+        """
+        Check is the passed arguments have valid values.
+
+        The method is intended to be overridden in a subclass.
+        It should either warn about invalid values and gracefully continue or
+        raise a `SystemExit` exception.
+
+        :param args: The parsed command line arguments
+        :param pkg_names: The set of package names
+        """
+        pass
+
+    def select_packages(self, *, args, decorators):
+        """
+        Identify the packages which should be skipped.
+
+        By default all package decorators are marked as "selected".
+
+        This method must be overridden in a subclass.
+
+        :param args: The parsed command line arguments
+        :param list decorators: The package decorators
+        """
+        raise NotImplementedError()
+
+
+def add_arguments(parser):
+    """
+    Add the command line arguments for the package selection extensions.
+
+    The function will call :function:`add_package_discovery_arguments` to add
+    the package discovery arguments.
+
+    :param parser: The argument parser
+    """
+    add_package_discovery_arguments(parser)
+
+    _add_package_selection_arguments(parser)
+
+
+def get_package_selection_extensions():
+    """
+    Get the available package selection extensions.
+
+    The extensions are ordered by their entry point name.
+
+    :rtype: OrderedDict
+    """
+    extensions = instantiate_extensions(__name__)
+    for name, extension in extensions.items():
+        extension.PACKAGE_SELECTION_NAME = name
+    return order_extensions_by_name(extensions)
+
+
+def _add_package_selection_arguments(parser):
+    """
+    Add the command line arguments for the package selection extensions.
+
+    :param parser: The argument parser
+    """
+    package_selection_extensions = get_package_selection_extensions()
+    group = parser.add_argument_group(title='Package selection arguments')
+    for extension in package_selection_extensions.values():
+        try:
+            retval = extension.add_arguments(parser=group)
+            assert retval is None, 'add_arguments() should return None'
+        except Exception as e:
+            # catch exceptions raised in package selection extension
+            exc = traceback.format_exc()
+            logger.error(
+                'Exception in package selection extension '
+                "'{extension.PACKAGE_SELECTION_NAME}': {e}\n{exc}"
+                .format_map(locals()))
+            # skip failing extension, continue with next one
+
+
+def get_packages(
+    args, *,
+    additional_argument_names=None,
+    direct_categories=None, recursive_categories=None
+):
+    """
+    Get the selected package decorators in topological order.
+
+    The overview of the process:
+    * Get the package descriptors
+    * Order them topologically
+    * Select the packages based on the command line arguments
+
+    :param additional_argument_names: A list of additional arguments to
+      consider
+    :param Iterable[str] direct_categories: The names of the direct categories
+    :param Iterable[str] recursive_categories: The names of the recursive
+      categories
+    :rtype: list
+    :raises RuntimeError: if the returned set of packages contains duplicates
+      package names
+    """
+    descriptors = get_package_descriptors(
+        args, additional_argument_names=additional_argument_names)
+    decorators = topological_order_packages(
+        descriptors,
+        direct_categories=direct_categories,
+        recursive_categories=recursive_categories)
+    select_package_decorators(args, decorators)
+
+    pkgs = [m.descriptor for m in decorators if m.selected]
+    if len({d.name for d in pkgs}) < len(pkgs):
+        raise RuntimeError('Duplicate package names not supported')
+
+    return decorators
+
+
+def get_package_descriptors(args, *, additional_argument_names=None):
+    """
+    Get the package descriptors.
+
+    The overview of the process:
+    * Discover the package descriptors using the package discovery and
+      identification extensions
+    * Check is the passed package selection arguments have valid values
+    * Augment the package descriptors
+
+    :param additional_argument_names: A list of additional arguments to
+      consider
+    :returns: set of
+      :py:class:`colcon_core.package_descriptor.PackageDescriptor`
+    :rtype: set
+    """
+    extensions = get_package_identification_extensions()
+    descriptors = discover_packages(args, extensions)
+
+    pkg_names = {d.name for d in descriptors}
+    _check_package_selection_parameters(args, pkg_names)
+
+    augment_packages(
+        descriptors, additional_argument_names=additional_argument_names)
+    return descriptors
+
+
+def _check_package_selection_parameters(args, pkg_names):
+    package_selection_extensions = get_package_selection_extensions()
+    for extension in package_selection_extensions.values():
+        try:
+            retval = extension.check_parameters(args=args, pkg_names=pkg_names)
+            assert retval is None, 'check_parameters() should return None'
+        except Exception as e:
+            # catch exceptions raised in package selection extension
+            exc = traceback.format_exc()
+            logger.error(
+                'Exception in package selection extension '
+                "'{extension.PACKAGE_SELECTION_NAME}': {e}\n{exc}"
+                .format_map(locals()))
+            # skip failing extension, continue with next one
+
+
+def select_package_decorators(args, decorators):
+    """
+    Select the package decorators based on the command line arguments.
+
+    The `selected` attribute of each decorator is updated by this function.
+
+    :param args: The parsed command line arguments
+    :param list decorators: The package decorators
+    """
+    # filtering must happen after the topological ordering since otherwise
+    # packages in the middle of the dependency graph might be missing
+    package_selection_extensions = get_package_selection_extensions()
+    for extension in package_selection_extensions.values():
+        try:
+            retval = extension.select_packages(
+                args=args, decorators=decorators)
+            assert retval is None, 'select_packages() should return None'
+        except Exception as e:
+            # catch exceptions raised in package selection extension
+            exc = traceback.format_exc()
+            logger.error(
+                'Exception in package selection extension '
+                "'{extension.PACKAGE_SELECTION_NAME}': {e}\n{exc}"
+                .format_map(locals()))
+            # skip failing extension, continue with next one
---
+++ test/test_package_selection.py
@@ -0,0 +1,189 @@
+# Copyright 2016-2018 Dirk Thomas
+# Licensed under the Apache License, Version 2.0
+
+from argparse import Namespace
+
+from colcon_core.package_descriptor import PackageDescriptor
+from colcon_core.package_selection import _add_package_selection_arguments
+from colcon_core.package_selection import _check_package_selection_parameters
+from colcon_core.package_selection import add_arguments
+from colcon_core.package_selection import get_package_selection_extensions
+from colcon_core.package_selection import get_packages
+from colcon_core.package_selection import PackageSelectionExtensionPoint
+from colcon_core.package_selection import select_package_decorators
+from mock import Mock
+from mock import patch
+import pytest
+
+from .entry_point_context import EntryPointContext
+
+
+class Extension1(PackageSelectionExtensionPoint):
+    pass
+
+
+class Extension2(PackageSelectionExtensionPoint):
+    pass
+
+
+def test_get_package_selection_extensions():
+    with EntryPointContext(extension1=Extension1, extension2=Extension2):
+        extensions = get_package_selection_extensions()
+        assert ['extension1', 'extension2'] == list(extensions.keys())
+
+
+def add_dummy_arguments(parser):
+    parser.add_argument('arg')
+
+
+def test__add_package_selection_arguments():
+    parser = Mock()
+    with EntryPointContext(extension1=Extension1, extension2=Extension2):
+        extensions = get_package_selection_extensions()
+
+        # invalid return value
+        extensions['extension1'].add_arguments = Mock(return_value=True)
+        with patch(
+            'colcon_core.package_selection.add_package_discovery_arguments'
+        ) as add_package_discovery_arguments:
+            with patch('colcon_core.package_selection.logger.error') as error:
+                add_arguments(parser)
+        assert add_package_discovery_arguments.call_count == 1
+        # the raised assertion is catched and results in an error message
+        assert error.call_count == 1
+        assert len(error.call_args[0]) == 1
+        assert error.call_args[0][0].startswith(
+            "Exception in package selection extension 'extension1': "
+            'add_arguments() should return None\n')
+
+        # raise exception
+        extensions['extension1'].add_arguments = Mock(
+            side_effect=RuntimeError('custom exception'))
+        # check that arguments can be added
+        extensions['extension2'].add_arguments = Mock(
+            side_effect=add_dummy_arguments)
+        with patch('colcon_core.package_selection.logger.error') as error:
+            _add_package_selection_arguments(parser)
+        assert extensions['extension2'].add_arguments.call_count == 1
+        # the raised exception is catched and results in an error message
+        assert error.call_count == 1
+        assert len(error.call_args[0]) == 1
+        assert error.call_args[0][0].startswith(
+            "Exception in package selection extension 'extension1': "
+            'custom exception\n')
+
+
+def test_get_packages():
+    args = Namespace()
+    d1 = PackageDescriptor('/some/path')
+    d1.name = 'one'
+    d2 = PackageDescriptor('/other/path')
+    d2.name = 'two'
+    with patch(
+        'colcon_core.package_selection.discover_packages',
+        return_value=[d1, d2]
+    ):
+        decos = get_packages(args)
+    assert len(decos) == 2
+    assert decos[0].descriptor.name == 'one'
+    assert decos[0].selected is True
+    assert decos[1].descriptor.name == 'two'
+    assert decos[1].selected is True
+
+    d2.name = 'one'
+    with patch(
+        'colcon_core.package_selection.discover_packages',
+        return_value=[d1, d2]
+    ):
+        with pytest.raises(RuntimeError) as e:
+            get_packages(args)
+        assert str(e).endswith('Duplicate package names not supported')
+
+
+def test__check_package_selection_parameters():
+    args = Mock()
+    pkg_names = Mock()
+
+    with EntryPointContext(extension1=Extension1, extension2=Extension2):
+        extensions = get_package_selection_extensions()
+
+        # nothing wrong with the arguments
+        _check_package_selection_parameters(args, pkg_names)
+
+        # raise exception
+        extensions['extension1'].check_parameters = Mock(
+            side_effect=RuntimeError('custom exception'))
+        extensions['extension2'].check_parameters = Mock(return_value=None)
+        with patch('colcon_core.package_selection.logger.error') as error:
+            _check_package_selection_parameters(args, pkg_names)
+        assert extensions['extension2'].check_parameters.call_count == 1
+        # the raised exception is catched and results in an error message
+        assert error.call_count == 1
+        assert len(error.call_args[0]) == 1
+        assert error.call_args[0][0].startswith(
+            "Exception in package selection extension 'extension1': custom "
+            'exception\n')
+
+        # invalid return value
+        extensions['extension1'].check_parameters = Mock(return_value=True)
+        extensions['extension2'].check_parameters.reset_mock()
+        with patch('colcon_core.package_selection.logger.error') as error:
+            _check_package_selection_parameters(args, pkg_names)
+        assert extensions['extension2'].check_parameters.call_count == 1
+        # the raised assertion is catched and results in an error message
+        assert error.call_count == 1
+        assert len(error.call_args[0]) == 1
+        assert error.call_args[0][0].startswith(
+            "Exception in package selection extension 'extension1': "
+            'check_parameters() should return None\n')
+
+        # select some packages
+        extensions['extension1'].check_parameters = Mock(
+            side_effect=SystemExit(1))
+        with pytest.raises(SystemExit):
+            _check_package_selection_parameters(args, pkg_names)
+
+
+def select_some_packages(*, args, decorators):
+    for i, decorator in enumerate(decorators):
+        decorator.selected = bool(i % 2)
+
+
+def test_select_package_decorators():
+    args = Mock()
+    deco1 = Mock()
+    deco1.selected = True
+    deco2 = Mock()
+    deco2.selected = True
+    decos = [deco1, deco2]
+
+    with EntryPointContext(extension1=Extension1, extension2=Extension2):
+        extensions = get_package_selection_extensions()
+
+        # raise exception
+        extensions['extension2'].select_packages = Mock(return_value=None)
+        with patch('colcon_core.package_selection.logger.error') as error:
+            select_package_decorators(args, decos)
+        # the raised exception is catched and results in an error message
+        assert error.call_count == 1
+        assert len(error.call_args[0]) == 1
+        assert error.call_args[0][0].startswith(
+            "Exception in package selection extension 'extension1': \n")
+
+        # invalid return value
+        extensions['extension1'].select_packages = Mock(return_value=True)
+        with patch('colcon_core.package_selection.logger.error') as error:
+            select_package_decorators(args, decos)
+        # the raised assertion is catched and results in an error message
+        assert error.call_count == 1
+        assert len(error.call_args[0]) == 1
+        assert error.call_args[0][0].startswith(
+            "Exception in package selection extension 'extension1': "
+            'select_packages() should return None\n')
+
+        # select some packages
+        extensions['extension1'].select_packages = Mock(
+            side_effect=select_some_packages)
+        select_package_decorators(args, decos)
+        assert not deco1.selected
+        assert deco2.selected
