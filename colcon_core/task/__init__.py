96efd58fd0158717d636bcea9633fb718435bbea
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2018-02-28 20:23:29 -0800

    add task extension point

---
+++ colcon_core/task/__init__.py
@@ -0,0 +1,260 @@
+# Copyright 2016-2018 Dirk Thomas
+# Licensed under the Apache License, Version 2.0
+
+import os
+import shutil
+import sys
+import traceback
+
+from colcon_core.event.command import Command
+from colcon_core.event.job import JobProgress
+from colcon_core.event.output import StderrLine
+from colcon_core.event.output import StdoutLine
+from colcon_core.logging import colcon_logger
+from colcon_core.plugin_system import instantiate_extensions
+from colcon_core.plugin_system import order_extensions_by_name
+from colcon_core.subprocess import run
+
+logger = colcon_logger.getChild(__name__)
+
+
+class TaskContext:
+    """The context provided to tasks."""
+
+    def __init__(self, *, pkg, args, dependencies):
+        """
+        Constructor.
+
+        :param pkg: The package descriptor
+        :param args: The parsed command line arguments
+        :param dependencies: The ordered dictionary mapping dependency names to
+          their paths
+        """
+        self.event_queue = None
+        self.pkg = pkg
+        self.args = args
+        self.dependencies = dependencies
+
+    def put_event_into_queue(self, event):
+        """
+        Post a message event into the event queue.
+
+        The method is not implemented by default and will be replaced at
+        runtime once the event queue is known.
+
+        :param event: The event
+        """
+        raise NotImplementedError()
+
+
+class TaskExtensionPoint:
+    """
+    The interface for task extensions.
+
+    A task extension performs a specific logic and/or command sequence.
+
+    For each instance the attributes `TASK_NAME` and `PACKAGE_TYPE` are being
+    set to the (parent) basename of the entry point registering the extension.
+    """
+
+    """The version of the task extension interface."""
+    EXTENSION_POINT_VERSION = '1.0'
+
+    def add_arguments(self, *, parser):
+        """
+        Add command line arguments specific to the task.
+
+        The method is intended to be overridden in a subclass.
+
+        :param parser: The argument parser
+        """
+        pass
+
+    def set_context(self, *, context):
+        """
+        Set the context before the task is being `__call__`-ed.
+
+        :param context: The task context
+        """
+        self.context = context
+
+    async def __call__(self, *args, **kwargs):
+        """
+        Execute the task extension logic.
+
+        This method relays the call to a method named `self.TASK_NAME` which
+        should be implemented in subclasses.
+
+        :returns: The return code
+        """
+        task_method = getattr(self, self.TASK_NAME)
+        return await task_method(*args, **kwargs)
+
+    def print(self, msg, *, file=None):
+        """
+        Post a message event into the event queue.
+
+        :param msg: The message
+        :param file: The sink to write the message to. An argument of `None` or
+          `sys.stdout` posts a `StdoutLine` event, `sys.stderr` posts a
+          `StderrLine` event.
+        """
+        if file is None or file == sys.stdout:
+            data = StdoutLine(msg + '\n')
+        elif file == sys.stderr:
+            data = StderrLine(msg + '\n')
+        else:
+            assert False, 'Unknown file object: ' + str(file)
+        self.context.put_event_into_queue(data)
+
+    def progress(self, message):
+        """
+        Post a progress event into the event queue.
+
+        :param msg: The message
+        """
+        self.context.put_event_into_queue(
+            JobProgress(self.context.pkg.name, message))
+
+
+async def check_call(
+    context, cmd, *, cwd=None, env=None, shell=False, use_pty=None
+):
+    """
+    Run the command described by cmd.
+
+    Post a `Command` event to the queue describing the exact invocation in
+    order to allow reproducing it.
+    All output to `stdout` and `stderr` is posted as `StdoutLine` and
+    `StderrLine` events to the event queue.
+
+    :param cmd: The command and its arguments
+    :param cwd: the working directory for the subprocess
+    :param env: a dictionary with environment variables
+    :param shell: whether to use the shell as the program to execute
+    :param use_pty: whether to use a pseudo terminal
+    :returns: the result of the completed process
+    """
+    def stdout_callback(line):
+        context.put_event_into_queue(StdoutLine(line))
+
+    def stderr_callback(line):
+        context.put_event_into_queue(StderrLine(line))
+
+    context.put_event_into_queue(
+        Command(cmd, cwd=cwd, env=env, shell=shell))
+    return await run(
+        cmd, stdout_callback, stderr_callback,
+        cwd=cwd, env=env, shell=shell, use_pty=use_pty)
+
+
+def get_task_extensions(task_name, *, unique_instance=False):
+    """
+    Get the available task extensions.
+
+    The extensions are ordered by their entry point name.
+
+    :param str task_name: The task name identifying a group of task extensions
+    :param bool unique_instance: The flag if the returned instances should be
+      unique or cached instances can be returned instead
+    :rtype: OrderedDict
+    """
+    extensions = instantiate_extensions(
+        '%s.%s' % (__name__, task_name), unique_instance=unique_instance)
+    for name in list(extensions.keys()):
+        extension = extensions[name]
+        assert hasattr(extension, task_name)
+        extension.TASK_NAME = task_name
+        extension.PACKAGE_TYPE = name
+    return order_extensions_by_name(extensions)
+
+
+def add_task_arguments(parser, task_name):
+    """
+    Add the command line arguments for the task extensions.
+
+    :param parser: The argument parser
+    :param str task_name: The task name identifying a group of task extensions
+    """
+    extensions = get_task_extensions(task_name, unique_instance=True)
+    for extension_name, extension in extensions.items():
+        group = parser.add_argument_group(
+            title="Arguments for '{extension_name}' packages"
+            .format_map(locals()))
+        try:
+            retval = extension.add_arguments(parser=group)
+            assert retval is None, 'add_arguments() should return None'
+        except Exception as e:
+            # catch exceptions raised in task extension
+            exc = traceback.format_exc()
+            logger.error(
+                'Exception in task extension '
+                "'{extension.TASK_NAME}.{extension.PACKAGE_TYPE}': {e}\n{exc}"
+                .format_map(locals()))
+            # skip failing extension, continue with next one
+
+
+def get_task_extension(task_name, package_type):
+    """
+    Get a specific task extension.
+
+    :param str task_name: The task name identifying a group of task extensions
+    :param str package_type: The package type identifying a task extension
+      within the group
+    :returns: The task extension
+    """
+    extensions = instantiate_extensions(
+        '%s.%s' % (__name__, task_name), unique_instance=True)
+    if package_type not in extensions:
+        return None
+    extension = extensions[package_type]
+    extension.TASK_NAME = task_name
+    extension.PACKAGE_TYPE = package_type
+    return extension
+
+
+def create_file(args, rel_path, *, content=None):
+    """
+    Create a file within the install base.
+
+    Creates the containing directory if necessary.
+
+    :param args: The parsed command line arguments containing the install base
+    :param str rel_path: The relative path of file
+    :param str content: The content of the created file
+    :returns: The task extension
+    """
+    dst = os.path.join(args.install_base, rel_path)
+    os.makedirs(os.path.dirname(dst), exist_ok=True)
+    with open(dst, 'w') as h:
+        if content is not None:
+            h.write(content)
+
+
+def install(args, rel_src, rel_dst):
+    """
+    Install or symlink a file.
+
+    Creates the containing directory if necessary.
+
+    :param args: The parsed command line arguments containing the source path
+      as well as the install base
+    :param str rel_src: The source path relative to the path
+    :param str rel_dst: The destination path relative to the install base
+    """
+    src = os.path.join(args.path, rel_src)
+    dst = os.path.join(args.install_base, rel_dst)
+    os.makedirs(os.path.dirname(dst), exist_ok=True)
+    if not args.symlink_install:
+        if os.path.islink(dst):
+            os.unlink(dst)
+        shutil.copy(src, dst)
+    else:
+        if os.path.exists(dst):
+            if not os.path.islink(dst) or not os.path.samefile(src, dst):
+                if os.path.isfile(dst):
+                    os.remove(dst)
+                if os.path.isdir(dst):
+                    shutil.rmtree(dst)
+        if not os.path.exists(dst):
+            os.symlink(src, dst)
---
+++ test/test_task.py
@@ -0,0 +1,252 @@
+# Copyright 2016-2018 Dirk Thomas
+# Licensed under the Apache License, Version 2.0
+
+import os
+from pathlib import Path
+import sys
+from tempfile import TemporaryDirectory
+
+from colcon_core.event.command import Command
+from colcon_core.event.job import JobProgress
+from colcon_core.event.output import StderrLine
+from colcon_core.event.output import StdoutLine
+from colcon_core.plugin_system import instantiate_extensions
+from colcon_core.task import add_task_arguments
+from colcon_core.task import check_call
+from colcon_core.task import create_file
+from colcon_core.task import get_task_extension
+from colcon_core.task import get_task_extensions
+from colcon_core.task import install
+from colcon_core.task import TaskContext
+from colcon_core.task import TaskExtensionPoint
+from mock import Mock
+from mock import patch
+import pytest
+
+from .entry_point_context import EntryPointContext
+from .run_until_complete import run_until_complete
+
+
+def test_context_interface():
+    context = TaskContext(pkg=None, args=None, dependencies=None)
+    with pytest.raises(NotImplementedError):
+        context.put_event_into_queue(None)
+
+
+class Extension(TaskExtensionPoint):
+    TASK_NAME = 'do'
+
+    async def do(self, *args, **kwargs):
+        self.progress('progress')
+        self.print('hello')
+        self.print('hello', file=sys.stdout)
+        self.print('world', file=sys.stderr)
+        with pytest.raises(AssertionError):
+            self.print('invalid file handle', file=False)
+        return 1
+
+
+def test_extension_interface():
+    context = Mock()
+
+    # capture events
+    events = []
+
+    def put_event_into_queue(event):
+        nonlocal events
+        events.append(event)
+
+    context.put_event_into_queue = put_event_into_queue
+
+    extension = Extension()
+    extension.set_context(context=context)
+    rc = run_until_complete(extension())
+    assert rc == 1
+
+    assert len(events) == 4
+    assert isinstance(events[0], JobProgress)
+    assert events[0].progress == 'progress'
+    assert isinstance(events[1], StdoutLine)
+    assert events[1].line == 'hello\n'
+    assert isinstance(events[2], StdoutLine)
+    assert events[2].line == 'hello\n'
+    assert isinstance(events[3], StderrLine)
+    assert events[3].line == 'world\n'
+
+
+# TODO figure out how to avoid the stderr output
+@pytest.mark.skip(
+    reason='Results in stderr output due to a UnicodeDecodeError for the '
+           'generated coverage files')
+def test_check_call():
+    context = Mock()
+    events = []
+
+    def put_event_into_queue(event):
+        nonlocal events
+        events.append(event)
+
+    context.put_event_into_queue = put_event_into_queue
+    cmd = [
+        sys.executable, '-c',
+        "import sys; print('hello'); print('world', file=sys.stderr)"]
+    coroutine = check_call(context, cmd)
+    completed_process = run_until_complete(coroutine)
+    assert completed_process.returncode == 0
+    assert len(events) == 3
+    assert isinstance(events[0], Command)
+    assert events[0].cmd == cmd
+    assert isinstance(events[1], StdoutLine)
+    assert events[1].line == b'hello\n'
+    assert isinstance(events[2], StderrLine)
+    assert events[2].line == b'world\n'
+
+
+class Extension1(TaskExtensionPoint):
+
+    def build(self, *args, **kwargs):
+        pass  # pragma: no cover
+
+
+class Extension2(TaskExtensionPoint):
+
+    def build(self, *args, **kwargs):
+        pass  # pragma: no cover
+
+
+def instantiate_extensions_without_cache(
+    group_name, *, exclude_names=None, unique_instance=False
+):
+    return instantiate_extensions(group_name)
+
+
+def test_add_task_arguments():
+    parser = Mock()
+    task_name = 'build'
+    with EntryPointContext(extension1=Extension1, extension2=Extension2):
+        with patch(
+            'colcon_core.task.instantiate_extensions',
+            side_effect=instantiate_extensions_without_cache
+        ):
+            extensions = get_task_extensions(task_name)
+            # one exception, one success
+            extensions['extension1'].add_arguments = Mock(
+                side_effect=RuntimeError('custom exception'))
+            with patch('colcon_core.task.logger.error') as error:
+                add_task_arguments(parser, task_name)
+            assert error.call_count == 1
+            assert len(error.call_args[0]) == 1
+            assert error.call_args[0][0].startswith(
+                "Exception in task extension 'build.extension1': custom "
+                'exception\n')
+
+            # invalid return value
+            extensions['extension1'].add_arguments = Mock()
+            extensions['extension2'].add_arguments = Mock(return_value=None)
+            with patch('colcon_core.task.logger.error') as error:
+                add_task_arguments(parser, task_name)
+            assert error.call_count == 1
+            assert len(error.call_args[0]) == 1
+            assert error.call_args[0][0].startswith(
+                "Exception in task extension 'build.extension1': "
+                'add_arguments() should return None\n')
+            assert extensions['extension2'].add_arguments.call_count == 1
+
+
+def test_get_task_extension():
+    task_name = 'build'
+    with EntryPointContext(extension1=Extension1, extension2=Extension2):
+        # request invalid extension
+        extension = get_task_extension(task_name, 'package_type')
+        assert extension is None
+
+        # request valid extension
+        extension = get_task_extension(task_name, 'extension2')
+        assert isinstance(extension, Extension2)
+
+
+def test_create_file():
+    with TemporaryDirectory(prefix='test_colcon_') as base_path:
+        args = Mock()
+        args.install_base = base_path
+
+        create_file(args, 'file.txt')
+        path = Path(base_path) / 'file.txt'
+        assert path.is_file()
+        assert path.read_text() == ''
+
+        create_file(args, 'path/file.txt', content='content')
+        path = Path(base_path) / 'path' / 'file.txt'
+        assert path.is_file()
+        assert path.read_text() == 'content'
+
+
+def test_install():
+    with TemporaryDirectory(prefix='test_colcon_') as base_path:
+        args = Mock()
+        args.path = os.path.join(base_path, 'path')
+        args.install_base = os.path.join(base_path, 'install')
+        args.symlink_install = False
+
+        # create source files
+        os.makedirs(args.path)
+        with open(os.path.join(args.path, 'source.txt'), 'w') as h:
+            h.write('content')
+        with open(os.path.join(args.path, 'source2.txt'), 'w') as h:
+            h.write('content2')
+
+        # copy file
+        install(args, 'source.txt', 'destination.txt')
+        path = Path(base_path) / 'install' / 'destination.txt'
+        assert path.is_file()
+        assert not path.is_symlink()
+        assert path.read_text() == 'content'
+
+        # skip all symlink tests on Windows for now
+        if sys.platform == 'win32':  # pragma: no cover
+            return
+
+        # symlink file, removing existing file
+        args.symlink_install = True
+        install(args, 'source.txt', 'destination.txt')
+        path = Path(base_path) / 'install' / 'destination.txt'
+        assert path.is_file()
+        assert path.is_symlink()
+        assert path.samefile(os.path.join(args.path, 'source.txt'))
+        assert path.read_text() == 'content'
+
+        # symlink other file, removing existing directory
+        os.remove(os.path.join(args.install_base, 'destination.txt'))
+        os.makedirs(os.path.join(args.install_base, 'destination.txt'))
+        install(args, 'source2.txt', 'destination.txt')
+        path = Path(base_path) / 'install' / 'destination.txt'
+        assert path.is_file()
+        assert path.is_symlink()
+        assert path.samefile(os.path.join(args.path, 'source2.txt'))
+        assert path.read_text() == 'content2'
+
+        # copy file, removing existing symlink
+        args.symlink_install = False
+        install(args, 'source.txt', 'destination.txt')
+        path = Path(base_path) / 'install' / 'destination.txt'
+        assert path.is_file()
+        assert not path.is_symlink()
+        assert path.read_text() == 'content'
+
+        # symlink file
+        os.remove(os.path.join(args.install_base, 'destination.txt'))
+        args.symlink_install = True
+        install(args, 'source.txt', 'destination.txt')
+        path = Path(base_path) / 'install' / 'destination.txt'
+        assert path.is_file()
+        assert path.is_symlink()
+        assert path.samefile(os.path.join(args.path, 'source.txt'))
+        assert path.read_text() == 'content'
+
+        # symlink file, same already existing
+        install(args, 'source.txt', 'destination.txt')
+        path = Path(base_path) / 'install' / 'destination.txt'
+        assert path.is_file()
+        assert path.is_symlink()
+        assert path.samefile(os.path.join(args.path, 'source.txt'))
+        assert path.read_text() == 'content'
