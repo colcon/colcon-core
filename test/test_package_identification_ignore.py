e63bf377fcbf183309bdb9ff9b49ae7bd1950849
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2018-03-09 22:32:37 -0800

    add package identification extension to ignore directories using a marker file

---
+++ test/test_package_identification_ignore.py
@@ -0,0 +1,24 @@
+# Copyright 2016-2018 Dirk Thomas
+# Licensed under the Apache License, Version 2.0
+
+from pathlib import Path
+from tempfile import TemporaryDirectory
+
+from colcon_core.package_identification import SkipLocationException
+from colcon_core.package_identification.ignore import IGNORE_MARKER
+from colcon_core.package_identification.ignore \
+    import IgnorePackageIdentification
+from mock import Mock
+import pytest
+
+
+def test_identify():
+    extension = IgnorePackageIdentification()
+    metadata = Mock()
+    with TemporaryDirectory(prefix='test_colcon_') as basepath:
+        metadata.path = Path(basepath)
+        assert extension.identify(metadata) is None
+
+        (metadata.path / IGNORE_MARKER).write_text('')
+        with pytest.raises(SkipLocationException):
+            extension.identify(metadata)
