3584f2f42ff15a6f83d3409588ceb92469397217
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2018-03-09 22:05:20 -0800

    add environment extension point

---
+++ colcon_core/environment/__init__.py
@@ -0,0 +1,150 @@
+# Copyright 2016-2018 Dirk Thomas
+# Licensed under the Apache License, Version 2.0
+
+from collections import Iterable
+import os
+from pathlib import Path
+import traceback
+
+from colcon_core.logging import colcon_logger
+from colcon_core.plugin_system import instantiate_extensions
+from colcon_core.plugin_system import order_extensions_by_priority
+from colcon_core.shell import get_shell_extensions
+
+logger = colcon_logger.getChild(__name__)
+
+
+class EnvironmentExtensionPoint:
+    """
+    The interface for environment extensions.
+
+    An environment extension creates environment hooks for a specific
+    environment variable and uses the shell extensions to generate scripts for
+    each supported shell.
+
+    For each instance the attribute `ENVIRONMENT_NAME` is being set to the
+    basename of the entry point registering the extension.
+    """
+
+    """The version of the environment extension interface."""
+    EXTENSION_POINT_VERSION = '1.0'
+
+    """The default priority of environment extensions."""
+    PRIORITY = 100
+
+    def create_environment_hooks(self, prefix_path, pkg_name):
+        """
+        Create the environment hooks for a package.
+
+        This method must be overridden in a subclass.
+
+        :param prefix_path: The prefix path of the package
+        :param pkg_name: The package name
+        :returns: iterable of generated hook paths
+        :rtype: Iterable
+        """
+        raise NotImplementedError()
+
+
+def get_environment_extensions():
+    """
+    Get the available environment extensions.
+
+    The extensions are ordered by their priority and entry point name.
+
+    :rtype: OrderedDict
+    """
+    extensions = instantiate_extensions(__name__)
+    for name in list(extensions.keys()):
+        extension = extensions[name]
+        extension.ENVIRONMENT_NAME = name
+    return order_extensions_by_priority(extensions)
+
+
+def create_environment_scripts(
+    pkg, args, *, default_hooks=None, additional_hooks=None
+):
+    """
+    Create the environment scripts for a package.
+
+    :param pkg: The package descriptor
+    :param args: The parsed command line arguments
+    :param list default_hooks: If none are parsed explicitly the hooks provided
+      by :function:`create_environment_hooks` are used
+    :param list additional_hooks: Any additional hooks which should be
+      referenced by the generated scripts
+    :returns: iterable of the generated hook paths
+    :rtype: Iterable
+    """
+    logger.log(1, 'create_environment_scripts()')
+    prefix_path = Path(args.install_base)
+
+    hooks = []
+    if default_hooks is None:
+        default_hooks = create_environment_hooks(prefix_path, pkg.name)
+    hooks += default_hooks
+    if additional_hooks:
+        hooks += additional_hooks
+    hooks += pkg.hooks
+
+    # ensure each hook is presented by a tuple
+    # with the first element being a relative path
+    # and the second element being a list of arguments
+    hook_tuples = []
+    for hook in hooks:
+        hook_args = []
+        if isinstance(hook, list) or isinstance(hook, tuple):
+            hook_args = hook[1:]
+            hook = hook[0]
+
+        if os.path.isabs(str(hook)):
+            hook = os.path.relpath(str(hook), start=str(prefix_path))
+
+        hook_tuples.append((hook, hook_args))
+
+    extensions = get_shell_extensions()
+    for priority in extensions.keys():
+        extensions_same_prio = extensions[priority]
+        for extension in extensions_same_prio.values():
+            try:
+                retval = extension.create_package_script(
+                    prefix_path, pkg.name, hook_tuples)
+                assert retval is None, \
+                    'create_package_script() should return None'
+            except Exception as e:
+                # catch exceptions raised in shell extension
+                exc = traceback.format_exc()
+                logger.error(
+                    "Exception in shell extension '{extension.SHELL_NAME}': "
+                    '{e}\n{exc}'.format_map(locals()))
+                # skip failing extension, continue with next one
+
+
+def create_environment_hooks(prefix_path, pkg_name):
+    """
+    Create the environment hooks for a package.
+
+    :param prefix_path: The prefix path of the package
+    :param pkg_name: The package name
+    :returns: iterable of generated hook paths
+    :rtype: Iterable
+    """
+    prefix_path = Path(prefix_path)
+    all_hooks = []
+    extensions = get_environment_extensions()
+    for extension in extensions.values():
+        try:
+            hooks = extension.create_environment_hooks(prefix_path, pkg_name)
+            assert isinstance(hooks, Iterable), \
+                'create_environment_hooks() should return an iterable'
+        except Exception as e:
+            # catch exceptions raised in environment extension
+            exc = traceback.format_exc()
+            logger.error(
+                'Exception in environment extension '
+                "'{extension.ENVIRONMENT_NAME}': {e}\n{exc}"
+                .format_map(locals()))
+            # skip failing extension, continue with next one
+            continue
+        all_hooks += hooks
+    return all_hooks
---
+++ test/test_environment.py
@@ -0,0 +1,113 @@
+# Copyright 2016-2018 Dirk Thomas
+# Licensed under the Apache License, Version 2.0
+
+from pathlib import Path
+from tempfile import TemporaryDirectory
+
+from colcon_core.environment import create_environment_hooks
+from colcon_core.environment import create_environment_scripts
+from colcon_core.environment import EnvironmentExtensionPoint
+from colcon_core.environment import get_environment_extensions
+from colcon_core.shell import get_shell_extensions
+from colcon_core.shell import ShellExtensionPoint
+from mock import Mock
+from mock import patch
+import pytest
+
+from .entry_point_context import EntryPointContext
+
+
+def test_extension_interface():
+    extension = EnvironmentExtensionPoint()
+    with pytest.raises(NotImplementedError):
+        extension.create_environment_hooks(None, None)
+
+
+class Extension1(EnvironmentExtensionPoint):
+
+    def create_environment_hooks(self, prefix_path, pkg_name):
+        return [
+            '{prefix_path}/share/{pkg_name}/hook/one.ext'.format_map(locals()),
+            '{prefix_path}/share/{pkg_name}/hook/two.ext'.format_map(locals()),
+        ]
+
+
+class Extension2(EnvironmentExtensionPoint):
+    PRIORITY = 110
+
+
+def test_get_environment_extensions():
+    with EntryPointContext(extension1=Extension1, extension2=Extension2):
+        extensions = get_environment_extensions()
+    assert list(extensions.keys()) == ['extension2', 'extension1']
+
+
+class Extension3(ShellExtensionPoint):
+    pass
+
+
+class Extension4(ShellExtensionPoint):
+    pass
+
+
+def test_create_environment_scripts():
+    pkg = Mock()
+    pkg.name = 'name'
+    pkg.hooks = []
+    args = Mock()
+    args.install_base = '/some/path'
+
+    # no hooks at all
+    with patch(
+        'colcon_core.environment.create_environment_hooks', return_value=[]
+    ):
+        with patch(
+            'colcon_core.environment.get_shell_extensions', return_value={}
+        ):
+            create_environment_scripts(pkg, args)
+
+    pkg.hooks = ['/some/path/subA']
+    with EntryPointContext(extension3=Extension3, extension4=Extension4):
+        extensions = get_shell_extensions()
+        # one invalid return value, one check correct hooks argument
+        extensions[100]['extension3'].create_package_script = Mock()
+        extensions[100]['extension4'].create_package_script = Mock(
+            return_value=None)
+        with patch('colcon_core.environment.logger.error') as error:
+            create_environment_scripts(
+                pkg, args, default_hooks=[('subB', )],
+                additional_hooks=[['subC', 'arg1', 'arg2']])
+        # the raised exception is catched and results in an error message
+        assert error.call_count == 1
+        assert len(error.call_args[0]) == 1
+        assert error.call_args[0][0].startswith(
+            "Exception in shell extension 'extension3': "
+            'create_package_script() should return None\n')
+        # check for correct hooks argument
+        mock = extensions[100]['extension4'].create_package_script
+        assert mock.call_count == 1
+        assert len(mock.call_args[0]) == 3
+        assert mock.call_args[0][0] == Path(args.install_base)
+        assert mock.call_args[0][1] == pkg.name
+        hook_tuples = mock.call_args[0][2]
+        assert len(hook_tuples) == 3
+        assert hook_tuples[0] == ('subB', ())
+        assert hook_tuples[1] == ('subC', ['arg1', 'arg2'])
+        assert hook_tuples[2] == ('subA', [])
+
+
+def test_create_environment_hooks():
+    with TemporaryDirectory(prefix='test_colcon_') as basepath:
+        with EntryPointContext(extension1=Extension1, extension2=Extension2):
+            with patch('colcon_core.environment.logger.error') as error:
+                hooks = create_environment_hooks(basepath, 'pkg_name')
+    assert len(hooks) == 2
+    assert hooks[0] == '{basepath}/share/pkg_name/hook/one.ext' \
+        .format_map(locals())
+    assert hooks[1] == '{basepath}/share/pkg_name/hook/two.ext' \
+        .format_map(locals())
+    # the raised exception is catched and results in an error message
+    assert error.call_count == 1
+    assert len(error.call_args[0]) == 1
+    assert error.call_args[0][0].startswith(
+        "Exception in environment extension 'extension2': \n")
