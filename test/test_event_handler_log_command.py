99b0ab773d5d332453705a7fb6f0bad5857b4b6a
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2018-03-09 22:41:25 -0800

    add event handler extension to print each command which is being invoked

---
+++ test/test_event_handler_log_command.py
@@ -0,0 +1,19 @@
+# Copyright 2016-2018 Dirk Thomas
+# Licensed under the Apache License, Version 2.0
+
+from colcon_core.event.command import Command
+from colcon_core.event_handler.log_command import LogCommandEventHandler
+from mock import patch
+
+
+def test_console_direct():
+    extension = LogCommandEventHandler()
+
+    with patch('colcon_core.event_handler.log_command.logger.debug') as debug:
+        event = Command(['executable'], cwd='/some/path')
+        extension((event, None))
+        assert debug.call_count == 1
+
+        debug.reset_mock()
+        extension(('unknown', None))
+        assert debug.call_count == 0
