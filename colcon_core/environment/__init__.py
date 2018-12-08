a9e1766d9970c441d8c44491d11760a4af1b2daa
brawner <brawner@gmail.com>
2018-12-07 23:19:19 -0800

    collections.abc (#136)

--- colcon_core/environment/__init__.py
+++ colcon_core/environment/__init__.py
@@ -1,7 +1,7 @@
 # Copyright 2016-2018 Dirk Thomas
 # Licensed under the Apache License, Version 2.0
 
-from collections import Iterable
+from collections.abc import Iterable
 import os
 from pathlib import Path
 import traceback
