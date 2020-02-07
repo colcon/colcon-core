09b16d6636c83f04831e78076ee5a6b935ce8377
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2020-02-07 15:20:58 -0800

    standardize behavior when passing an empty string (#304)

--- colcon_core/shell/sh.py
+++ colcon_core/shell/sh.py
@@ -87,6 +87,8 @@ class ShShell(ShellExtensionPoint):
         hook_path = prefix_path / 'share' / pkg_name / 'hook' / \
             ('%s.sh' % env_hook_name)
         logger.info("Creating environment hook '%s'" % hook_path)
+        if value == '':
+            value = '$COLCON_PREFIX_PATH'
         expand_template(
             Path(__file__).parent / 'template' / 'hook_set_value.sh.em',
             hook_path, {'name': name, 'value': value})
