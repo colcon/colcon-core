99110b2d392f14764137589d0ca7e23c17036811
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2020-07-17 09:43:53 -0700

    avoid warning introduced in setuptools 49.2.0 (#361)

--- colcon_core/task/python/build.py
+++ colcon_core/task/python/build.py
@@ -2,6 +2,10 @@
 # Licensed under the Apache License, Version 2.0
 
 from contextlib import suppress
+with suppress(ImportError):
+    # needed before importing distutils
+    # to avoid warning introduced in setuptools 49.2.0
+    import setuptools  # noqa: F401
 from distutils.sysconfig import get_python_lib
 import os
 from pathlib import Path
