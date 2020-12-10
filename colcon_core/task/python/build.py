colcon/colcon-core

commit 6e101f379e87e40001be5ef2b349176d8fe6027e

fix regression when package dir is symlinked (#406)

colcon_core/task/python/build.py
@@ -75,9 +75,11 @@ class PythonBuildTask(TaskExtensionPoint):
             if 'egg_info' in available_commands:
                 # `setup.py egg_info` requires the --egg-base to exist
                 os.makedirs(args.build_base, exist_ok=True)
+                # symlinks are resolved for args.path when used as cwd below
                 cmd += [
                     'egg_info', '--egg-base',
-                    os.path.relpath(args.build_base, args.path)]
+                    os.path.relpath(
+                        args.build_base, os.path.realpath(args.path))]
             cmd += [
                 'build', '--build-base', os.path.join(
                     args.build_base, 'build'),

commit 823603045b7b5c18b4cc48efa2dd30428e6114d1

Fix regression when enitre workspace is symlinked
This bug can be demonstrated using the macOS Catalina virtual
environments in GitHub Actions, where it appears that the working
directory is under a symlink.

Regression caused by 6e101f379e87e40001be5ef2b349176d8fe6027e

colcon_core/task/python/build.py
@@ -75,11 +75,12 @@ class PythonBuildTask(TaskExtensionPoint):
             if 'egg_info' in available_commands:
                 # `setup.py egg_info` requires the --egg-base to exist
                 os.makedirs(args.build_base, exist_ok=True)
-                # symlinks are resolved for args.path when used as cwd below
+                # symlinks are resolved for the paths when used as cwd below
                 cmd += [
                     'egg_info', '--egg-base',
                     os.path.relpath(
-                        args.build_base, os.path.realpath(args.path))]
+                        os.path.realpath(args.build_base),
+                        os.path.realpath(args.path))]
             cmd += [
                 'build', '--build-base', os.path.join(
                     args.build_base, 'build'),