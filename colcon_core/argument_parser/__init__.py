b7e5779f08a273596e1980099f5fc61ea90b9c8e
Scott K Logan <logans@cottsay.net>
2020-12-11 18:26:30 -0800

    Use inheritance to call ArgumentParser.add_argument
    This will make the behavior of inheriting argument parser decoration
    classes more predictable when using multiple inheritance.

--- colcon_core/argument_parser/__init__.py
+++ colcon_core/argument_parser/__init__.py
@@ -161,6 +161,15 @@ class ArgumentParserDecorator:
         # get attribute on decoree
         setattr(self.__dict__['_parser'], name, value)
 
+    def add_argument(self, *args, **kwargs):
+        """
+        Add an argument.
+
+        See :class:`argparse.ArgumentParser.add_argument` for the method
+        arguments and return value.
+        """
+        return self._parser.add_argument(*args, **kwargs)
+
     def add_argument_group(self, *args, **kwargs):
         """
         Decorate group parser before adding.
