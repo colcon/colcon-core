colcon/colcon-core

commit a1018464a69a46f9f8266646176f8e001221d25c

Extract test dependencies from [options.extras_require]test (#450)
It's becoming an increasingly common pattern to declare test
dependencies using [options.extras_require]test. This change merges the
test dependencies declared using that mechanism with any discovered
using [options]test_requires.

colcon_core/package_augmentation/python.py
@@ -75,12 +75,26 @@ def extract_dependencies(options):
         'tests_require': 'test',
     }
     dependencies = {}
+    _map_dependencies(options, mapping, dependencies)
+
+    extras_mapping = {
+        'test': 'test',
+        'tests': 'test',
+        'testing': 'test',
+    }
+    _map_dependencies(
+        options.get('extras_require', {}), extras_mapping,
+        dependencies)
+
+    return dependencies
+
+
+def _map_dependencies(options, mapping, dependencies):
     for option_name, dependency_type in mapping.items():
-        dependencies[dependency_type] = set()
+        dependencies.setdefault(dependency_type, set())
         for dep in options.get(option_name, []):
             dependencies[dependency_type].add(
                 create_dependency_descriptor(dep))
-    return dependencies
 
 
 def create_dependency_descriptor(requirement_string):

test/test_package_identification_python.py
@@ -64,7 +64,12 @@ def test_identify():
             '  runA > 1.2.3\n'
             '  runB\n'
             'tests_require = test == 2.0.0\n'
-            'zip_safe = false\n')
+            'zip_safe = false\n'
+            '[options.extras_require]\n'
+            'test = test2 == 3.0.0\n'
+            'tests = test3\n'
+            'testing = test4\n'
+            'other = not-test\n')
         assert extension.identify(desc) is None
         assert desc.name == 'other-name'
         assert desc.type == 'python'
@@ -77,7 +82,7 @@ def test_identify():
         assert desc.dependencies['run'] == {'runA', 'runB'}
         dep = next(x for x in desc.dependencies['run'] if x == 'runA')
         assert dep.metadata['version_gt'] == '1.2.3'
-        assert desc.dependencies['test'] == {'test'}
+        assert desc.dependencies['test'] == {'test', 'test2', 'test3', 'test4'}
 
         assert callable(desc.metadata['get_python_setup_options'])
         options = desc.metadata['get_python_setup_options'](None)

colcon_core/task/python/test/__init__.py
@@ -6,6 +6,7 @@ import traceback
 
 from colcon_core.entry_point import load_entry_points
 from colcon_core.logging import colcon_logger
+from colcon_core.package_augmentation.python import extract_dependencies
 from colcon_core.plugin_system import get_first_line_doc
 from colcon_core.plugin_system import instantiate_extensions
 from colcon_core.plugin_system import order_extensions_by_priority
@@ -222,7 +223,7 @@ def has_test_dependency(setup_py_data, name):
       False otherwise
     :rtype: bool
     """
-    tests_require = setup_py_data.get('tests_require')
+    tests_require = extract_dependencies(setup_py_data).get('test')
     for d in tests_require or []:
         # the name might be followed by a version
         # separated by any of the following: ' ', <, >, <=, >=, ==, !=

test/test_task_python_test_pytest.py
@@ -1,0 +1,49 @@
+# Copyright 2021 Open Source Robotics Foundation, Inc.
+# Licensed under the Apache License, Version 2.0
+
+from colcon_core.package_descriptor import PackageDescriptor
+from colcon_core.task import TaskContext
+from colcon_core.task.python import get_setup_data
+from colcon_core.task.python.test.pytest import PytestPythonTestingStep
+
+
+def test_pytest_match():
+    extension = PytestPythonTestingStep()
+    env = {}
+    desc = PackageDescriptor('/dev/null')
+    context = TaskContext(pkg=desc, args=None, dependencies=None)
+
+    desc.name = 'pkg-name'
+    desc.type = 'python'
+
+    # no test requirements
+    desc.metadata['get_python_setup_options'] = lambda env: {}
+    assert not extension.match(context, env, get_setup_data(desc, env))
+
+    # pytest not in tests_require
+    desc.metadata['get_python_setup_options'] = lambda env: {
+        'tests_require': ['nose'],
+    }
+    assert not extension.match(context, env, get_setup_data(desc, env))
+
+    # pytest not in extras_require.test
+    desc.metadata['get_python_setup_options'] = lambda env: {
+        'extras_require': {
+            'test': ['nose']
+        },
+    }
+    assert not extension.match(context, env, get_setup_data(desc, env))
+
+    # pytest in tests_require
+    desc.metadata['get_python_setup_options'] = lambda env: {
+        'tests_require': ['pytest'],
+    }
+    assert extension.match(context, env, get_setup_data(desc, env))
+
+    # pytest in extras_require.test
+    desc.metadata['get_python_setup_options'] = lambda env: {
+        'extras_require': {
+            'test': ['pytest']
+        },
+    }
+    assert extension.match(context, env, get_setup_data(desc, env))

commit 06c0b286782ff87dfa78e97ed50595dd64612606

Handle 'None' in options.extras_require
I'm not sure the precise circumstances that lead to this, but I'm seeing
'None' coming out of options.extras_require entries in the wild.

Regression caused by a1018464a69a46f9f8266646176f8e001221d25c

colcon_core/package_augmentation/python.py
@@ -83,7 +83,7 @@ def extract_dependencies(options):
         'testing': 'test',
     }
     _map_dependencies(
-        options.get('extras_require', {}), extras_mapping,
+        options.get('extras_require') or {}, extras_mapping,
         dependencies)
 
     return dependencies
@@ -92,7 +92,7 @@ def extract_dependencies(options):
 def _map_dependencies(options, mapping, dependencies):
     for option_name, dependency_type in mapping.items():
         dependencies.setdefault(dependency_type, set())
-        for dep in options.get(option_name, []):
+        for dep in options.get(option_name) or []:
             dependencies[dependency_type].add(
                 create_dependency_descriptor(dep))
 

test/test_package_identification_python.py
@@ -46,7 +46,16 @@ def test_identify():
         assert extension.identify(desc) is None
         assert desc.name == 'pkg-name'
         assert desc.type == 'python'
+        assert not desc.dependencies
+        assert not desc.metadata
 
+        augmentation_extension.augment_package(desc)
+        assert set(desc.dependencies.keys()) == {'build', 'run', 'test'}
+        assert not desc.dependencies['build']
+        assert not desc.dependencies['run']
+        assert not desc.dependencies['test']
+
+        desc = PackageDescriptor(basepath)
         desc.name = 'other-name'
         with pytest.raises(RuntimeError) as e:
             extension.identify(desc)