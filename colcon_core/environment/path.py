d22fb5e008e24dfc3e8a5d9322cbca12a7e38b83
Shane Loretz <sloretz@osrfoundation.org>
2022-04-20 12:00:58 -0700

    Add Python scripts path to PATH
    Signed-off-by: Shane Loretz <sloretz@osrfoundation.org>

--- colcon_core/environment/path.py
+++ colcon_core/environment/path.py
@@ -45,3 +45,44 @@ class PathEnvironment(EnvironmentExtensionPoint):
                 break
 
         return hooks
+
+
+
+class PythonScriptsPathEnvironment(EnvironmentExtensionPoint):
+    """Extend the `PATH` variable to find python scripts."""
+
+    def __init__(self):  # noqa: D107
+        super().__init__()
+        satisfies_version(
+            EnvironmentExtensionPoint.EXTENSION_POINT_VERSION, '^1.0')
+
+    def create_environment_hooks(self, prefix_path, pkg_name):  # noqa: D102
+        hooks = self._create_environment_hooks(prefix_path, pkg_name, 'bin')
+        if sys.platform == 'win32':
+            hooks += self._create_environment_hooks(
+                prefix_path, pkg_name, 'Scripts', '-scripts')
+        return hooks
+
+    def _create_environment_hooks(
+        self, prefix_path, pkg_name, subdirectory, suffix=''
+    ):
+        hooks = []
+        bin_path = Path(
+            sysconfig.get_path(
+                'scripts', vars={'base': prefix_path}, scheme='posix_prefix'))
+        logger.log(1, "checking '%s'" % bin_path)
+        try:
+            names = os.listdir(str(bin_path))
+        except FileNotFoundError:
+            pass
+        else:
+            for name in names:
+                if not (bin_path / name).is_file():
+                    continue
+                rel_bin_path = bin_path.relative_to(prefix_path)
+                hooks += shell.create_environment_hook(
+                    'path' + suffix, prefix_path, pkg_name, 'PATH',
+                    str(rel_bin_path), mode='prepend')
+                break
+
+        return hooks
