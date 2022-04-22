0ba2ba6ad8400dfeb7a19aa44a117412143a8c63
Shane Loretz <sloretz@osrfoundation.org>
2022-04-22 14:15:06 -0700

    Simplify path hook logic
    Signed-off-by: Shane Loretz <sloretz@osrfoundation.org>

--- colcon_core/environment/path.py
+++ colcon_core/environment/path.py
@@ -11,6 +11,18 @@ from colcon_core.plugin_system import satisfies_version
 from colcon_core.python_install_path import get_python_install_path
 
 
+def _has_file(path):
+    logger.log(1, "checking '%s'" % path)
+
+    if not path.is_dir():
+        return False
+
+    for child in path.iterdir():
+        if child.is_file():
+            return True
+    return False
+
+
 class PathEnvironment(EnvironmentExtensionPoint):
     """Extend the `PATH` variable to find executables."""
 
@@ -20,30 +32,18 @@ class PathEnvironment(EnvironmentExtensionPoint):
             EnvironmentExtensionPoint.EXTENSION_POINT_VERSION, '^1.0')
 
     def create_environment_hooks(self, prefix_path, pkg_name):  # noqa: D102
-        hooks = self._create_environment_hooks(prefix_path, pkg_name, 'bin')
-        if sys.platform == 'win32':
-            hooks += self._create_environment_hooks(
-                prefix_path, pkg_name, 'Scripts', '-scripts')
-        return hooks
+        return self._create_environment_hooks(prefix_path, pkg_name, 'bin')
 
     def _create_environment_hooks(
         self, prefix_path, pkg_name, subdirectory, suffix=''
     ):
         hooks = []
         bin_path = prefix_path / subdirectory
-        logger.log(1, "checking '%s'" % bin_path)
-        try:
-            names = os.listdir(str(bin_path))
-        except FileNotFoundError:
-            pass
-        else:
-            for name in names:
-                if not (bin_path / name).is_file():
-                    continue
-                hooks += shell.create_environment_hook(
-                    'path' + suffix, prefix_path, pkg_name, 'PATH',
-                    subdirectory, mode='prepend')
-                break
+
+        if _has_file(bin_path):
+            hooks += shell.create_environment_hook(
+                'path' + suffix, prefix_path, pkg_name, 'PATH', subdirectory,
+                mode='prepend')
 
         return hooks
 
@@ -57,30 +57,18 @@ class PythonScriptsPathEnvironment(EnvironmentExtensionPoint):
             EnvironmentExtensionPoint.EXTENSION_POINT_VERSION, '^1.0')
 
     def create_environment_hooks(self, prefix_path, pkg_name):  # noqa: D102
-        hooks = self._create_environment_hooks(prefix_path, pkg_name, 'bin')
-        if sys.platform == 'win32':
-            hooks += self._create_environment_hooks(
-                prefix_path, pkg_name, 'Scripts', '-scripts')
-        return hooks
-
-    def _create_environment_hooks(
-        self, prefix_path, pkg_name, subdirectory, suffix=''
-    ):
         hooks = []
         bin_path = get_python_install_path('scripts', {'base': prefix_path})
         logger.log(1, "checking '%s'" % bin_path)
         try:
             names = os.listdir(str(bin_path))
         except FileNotFoundError:
-            pass
-        else:
-            for name in names:
-                if not (bin_path / name).is_file():
-                    continue
-                rel_bin_path = bin_path.relative_to(prefix_path)
-                hooks += shell.create_environment_hook(
-                    'path' + suffix, prefix_path, pkg_name, 'PATH',
-                    str(rel_bin_path), mode='prepend')
-                break
+            return
+
+        if _has_file(bin_path):
+            rel_bin_path = bin_path.relative_to(prefix_path)
+            hooks += shell.create_environment_hook(
+                'pythonscriptspath' + suffix, prefix_path, pkg_name, 'PATH',
+                str(rel_bin_path), mode='prepend')
 
         return hooks
