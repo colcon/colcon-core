6eea111faab17fd2deb1ad3a27e704aea5eeb5d3
Shane Loretz <sloretz@osrfoundation.org>
2022-04-22 12:14:17 -0700

    Fix indentation
    Signed-off-by: Shane Loretz <sloretz@osrfoundation.org>

--- colcon_core/python_install_path.py
+++ colcon_core/python_install_path.py
@@ -18,7 +18,7 @@ def get_python_install_path(name, vars_=None):
     """
     kwargs = {}
     if vars_:
-      kwargs['vars'] = vars_
+        kwargs['vars'] = vars_
     if 'deb_system' in sysconfig.get_scheme_names():
         kwargs['scheme'] = 'deb_system'
         
