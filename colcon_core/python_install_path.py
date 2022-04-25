2dadb59a19aff6d1d88ab7ca8339b61d697ee971
Shane Loretz <sloretz@openrobotics.org>
2022-04-25 10:49:38 -0700

    Use deb_system scheme when available to avoid 'local' in path (#504)
    * Add Python scripts path to PATH
    * Use deb_system scheme if available
    * Add a unit test for PythonScriptsPathEnvironment
    
    Signed-off-by: Shane Loretz <sloretz@osrfoundation.org>
    Co-authored-by: Scott K Logan <logans@cottsay.net>

---
+++ colcon_core/python_install_path.py
@@ -0,0 +1,24 @@
+# Copyright 2022 Open Source Robotics Foundation, Inc.
+# Licensed under the Apache License, Version 2.0
+
+from pathlib import Path
+import sysconfig
+
+
+def get_python_install_path(name, vars_=()):
+    """
+    Get Python install paths matching Colcon's preferred scheme.
+
+    See sysconfig.get_path for more info about the arguments.
+
+    :param name: Name of the path type
+    :param vars_: A dictionary of variables updating the values of
+        sysconfig.get_config_vars()
+    :rtype: Pathlib.Path
+    """
+    kwargs = {}
+    kwargs['vars'] = dict(vars_)
+    if 'deb_system' in sysconfig.get_scheme_names():
+        kwargs['scheme'] = 'deb_system'
+
+    return Path(sysconfig.get_path(name, **kwargs))
--- test/test_environment_pythonpath.py
+++ test/test_environment_pythonpath.py
@@ -2,11 +2,11 @@
 # Licensed under the Apache License, Version 2.0
 
 from pathlib import Path
-import sysconfig
 from tempfile import TemporaryDirectory
 from unittest.mock import patch
 
 from colcon_core.environment.pythonpath import PythonPathEnvironment
+from colcon_core.python_install_path import get_python_install_path
 
 
 def test_pythonpath():
@@ -23,8 +23,8 @@ def test_pythonpath():
             assert len(hooks) == 0
 
             # Python path exists
-            python_path = Path(
-                sysconfig.get_path('purelib', vars={'base': prefix_path}))
+            python_path = get_python_install_path(
+                'purelib', {'base': prefix_path})
             python_path.mkdir(parents=True)
             hooks = extension.create_environment_hooks(prefix_path, 'pkg_name')
             assert len(hooks) == 2
