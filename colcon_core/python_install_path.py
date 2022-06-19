e65ae4d281f8cb0e8a8e172b5615b62038912640
Scott K Logan <logans@cottsay.net>
2022-06-19 11:26:21 -0700

    Add workaround for Fedora's rpm_prefix distribution patch (#510)
    Similar to what Debian did when Ubuntu Jammy shipped, Fedora has patched
    CPython to override the installation directory for Python packages,
    presumably to avoid collision with the system-packaged Python modules.
    
    Fedora's approach is slightly different, but can be patched around in a
    similar way.

--- colcon_core/python_install_path.py
+++ colcon_core/python_install_path.py
@@ -23,5 +23,11 @@ def get_python_install_path(name, vars_=()):
     if 'deb_system' in sysconfig.get_scheme_names() or \
             'osx_framework_library' in sysconfig.get_scheme_names():
         kwargs['scheme'] = 'posix_prefix'
+    # The presence of the rpm_prefix scheme indicates that posix_prefix
+    # has been patched to inject `local` into the installation locations.
+    # The rpm_prefix scheme is a backup of what posix_prefix was before it was
+    # patched.
+    elif 'rpm_prefix' in sysconfig.get_scheme_names():
+        kwargs['scheme'] = 'rpm_prefix'
 
     return Path(sysconfig.get_path(name, **kwargs))
