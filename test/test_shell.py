e6cd3de28929b952d0f8ac82b3e4827341a524d2
Scott K Logan <logans@cottsay.net>
2022-11-16 15:49:37 -0800

    Stop using 'echo' in shell test (#534)
    On Linux, 'echo' is an executable. On Windows, 'echo' is a shell
    command. Even if we enable the shell while executing this test, the
    command acts differently on Windows and it is difficult to get it to
    print newlines as is required by this test.
    
    In any case, we can just use the python executable to print the text
    instead, which is obviously available during testing.

--- test/test_shell.py
+++ test/test_shell.py
@@ -4,6 +4,7 @@
 from collections import OrderedDict
 import os
 from pathlib import Path
+import sys
 from tempfile import TemporaryDirectory
 from unittest.mock import Mock
 from unittest.mock import patch
@@ -124,7 +125,8 @@ def test_get_command_environment():
 
 def test_get_environment_variables():
     cmd = [
-        'echo', 'FOO\nNAME=value\n\nSOMETHING\nNAME2=value with spaces']
+        sys.executable, '-c',
+        r'print("FOO\nNAME=value\n\nSOMETHING\nNAME2=value with spaces")']
 
     coroutine = get_environment_variables(cmd, shell=False)
     env = run_until_complete(coroutine)
