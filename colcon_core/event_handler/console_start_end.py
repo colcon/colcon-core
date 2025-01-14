dd3cb66d994f5c099d0b47bfe62a0f128fc55ffe
Scott K Logan <logans@cottsay.net>
2025-01-13 21:37:19 -0600

    More colors work

--- colcon_core/event_handler/console_start_end.py
+++ colcon_core/event_handler/console_start_end.py
@@ -37,7 +37,8 @@ class ConsoleStartEndEventHandler(EventHandlerExtensionPoint):
         if isinstance(data, JobStarted):
             job_id = Style.PackageOrJobName(data.identifier)
             self._start_times[data.identifier] = time.monotonic()
-            print(Style.SectionStart(f'Starting >>> {job_id}'), flush=True)
+            msg = 'Starting ' + Style.Pictogram('>>>') + f' {job_id}'
+            print(Style.SectionStart(msg), flush=True)
 
         elif isinstance(data, TestFailure):
             job = event[1]
@@ -47,22 +48,25 @@ class ConsoleStartEndEventHandler(EventHandlerExtensionPoint):
             job_id = Style.PackageOrJobName(data.identifier)
             duration = \
                 time.monotonic() - self._start_times[data.identifier]
-            duration_string = format_duration(duration)
+            duration_string = Style.Measurement(format_duration(duration))
             if not data.rc:
-                msg = f'Finished <<< {job_id} [{duration_string}]'
+                msg = 'Finished ' + Style.Pictogram('<<<') + \
+                    f' {job_id} [{duration_string}]'
                 job = event[1]
                 if job in self._with_test_failures:
                     msg += Style.Warning('\t[ with test failures ]')
                 writable = sys.stdout
 
             elif data.rc == SIGINT_RESULT:
-                msg = Style.Warning('Aborted') + f'  <<< {job_id} ' \
-                    f'[{duration_string}]'
+                msg = Style.Warning('Aborted') + '  ' + \
+                    Style.Pictogram('<<<') + ' {job_id} [{duration_string}]'
                 writable = sys.stdout
 
             else:
-                msg = Style.Error('Failed') + f'   <<< {job_id} ' \
-                    f'[{duration_string}, exited with code {data.rc}]'
+                msg = Style.Critical('Failed') + '   ' + \
+                    Style.Pictogram('<<<') + f' {job_id} ' \
+                    f'[{duration_string}, ' + \
+                    Style.Error(f'exited with code {data.rc}') + ']'
                 writable = sys.stderr
 
             print(Style.SectionEnd(msg), file=writable, flush=True)
--- colcon_core/output_style/ansi_colors.py
+++ colcon_core/output_style/ansi_colors.py
@@ -1,6 +1,8 @@
 # Copyright 2024 Open Source Robotics Foundation, Inc.
 # Licensed under the Apache License, Version 2.0
 
+import os
+
 from colcon_core.output_style import OutputStyleExtensionPoint
 from colcon_core.output_style import Stylizer
 from colcon_core.plugin_system import satisfies_version
@@ -12,7 +14,9 @@ class AnsiEscape(Stylizer):
     __slots__ = ()
 
     def __new__(cls, start, end=0):  # noqa: D102
-        return super().__new__(cls, f'\033[{start}m', f'\033[{end}m')
+        return super().__new__(
+            cls, f'\033[{start}m',
+            f'\033[{end}m' if end is not None else '')
 
     def __add__(self, other):  # noqa: D105
         if not isinstance(other, AnsiEscape):
@@ -23,17 +27,20 @@ class AnsiEscape(Stylizer):
 
     @staticmethod
     def _combine(first, second):
+        if not second or first == second:
+            return first
+        elif not first:
+            return second
         first = first[2:-1]
         second = second[2:-1]
-        if first == second:
-            return first
         return f'{first};{second}'
 
 
 AnsiEscape.Black = AnsiEscape(30)
 AnsiEscape.Blue = AnsiEscape(34)
-AnsiEscape.Bold = AnsiEscape(1, 22)
+AnsiEscape.Bright = AnsiEscape(1, 22)
 AnsiEscape.Cyan = AnsiEscape(36)
+AnsiEscape.Default = AnsiEscape(39, None) + AnsiEscape(49, None)
 AnsiEscape.Faint = AnsiEscape(2, 22)
 AnsiEscape.Flashing = AnsiEscape(5, 25)
 AnsiEscape.Green = AnsiEscape(32)
@@ -47,7 +54,7 @@ AnsiEscape.White = AnsiEscape(37)
 AnsiEscape.Yellow = AnsiEscape(33)
 
 
-class ColorsOutputStyle(OutputStyleExtensionPoint):
+class AnsiColorsOutputStyle(OutputStyleExtensionPoint):
     """Basic ANSI colorizing for console output."""
 
     def __init__(self):  # noqa: D107
@@ -56,12 +63,20 @@ class ColorsOutputStyle(OutputStyleExtensionPoint):
             OutputStyleExtensionPoint.EXTENSION_POINT_VERSION, '^1.0')
 
     def apply_style(self, style):  # noqa: D102
-        style.Critical = AnsiEscape(91)
+        if os.environ.get('NO_COLOR') not in (
+            None, '', '0', 'no', 'No', 'NO', 'false', 'False', 'FALSE',
+        ):
+            return
+
+        style.Critical = AnsiEscape.Bright + AnsiEscape.Red
+        style.Default = AnsiEscape.Default
         style.Error = AnsiEscape.Red
-        style.PackageOrJobName = AnsiEscape.Italic + AnsiEscape.Blue
-        style.SectionStart = AnsiEscape.Cyan
-        style.SectionEnd = AnsiEscape.Magenta
-        style.Strong = AnsiEscape.Bold
+        style.Measurement = AnsiEscape.Yellow
+        style.PackageOrJobName = AnsiEscape.Cyan
+        style.Pictogram = AnsiEscape.Bright + AnsiEscape.Green
+        style.SectionStart = AnsiEscape.Default
+        style.SectionEnd = AnsiEscape.Bright + AnsiEscape.Black
+        style.Strong = AnsiEscape.Bright
         style.Success = AnsiEscape.Green
         style.Warning = AnsiEscape.Yellow
         style.Weak = AnsiEscape.Faint
