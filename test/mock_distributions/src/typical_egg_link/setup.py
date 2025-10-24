4afc8be51041c1a327964a16fd083ed3f3141b7c
Scott K Logan <logans@cottsay.net>
2025-10-23 23:45:00 -0500

    WIP: Add functionality for enumerating files in packages

---
+++ test/mock_distributions/src/typical_egg_link/setup.py
@@ -0,0 +1,18 @@
+# Copyright 2025 Open Source Robotics Foundation, Inc.
+# Licensed under the Apache License, Version 2.0
+
+from setuptools import setup
+
+
+setup(
+    name='typical-egg-link',
+    version='0.0.0',
+    packages=[
+        'typical_egg_link',
+    ],
+    entry_points={
+        'console_scripts': [
+            'typical_egg_link = typical_egg_link:main',
+        ],
+    },
+)
