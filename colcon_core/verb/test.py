b2008de0505452f4cf338a6a23e3c1c205a4fbf0
Scott K Logan <logans@cottsay.net>
2026-09-09 14:05:51 -0500

    Fix test verb as well

--- colcon_core/verb/test.py
+++ colcon_core/verb/test.py
@@ -90,12 +90,14 @@ class TestVerb(VerbExtensionPoint):
             '--build-base',
             default=wrap_default_value('build'),
             type=get_cwd_path_resolver(),
-            help='The base path for all build directories (default: build)')
+            help='The base path for all build directories '
+                 '(default: %(default)s)')
         parser.add_argument(
             '--install-base',
             default=wrap_default_value('install'),
             type=get_cwd_path_resolver(),
-            help='The base path for all install prefixes (default: install)')
+            help='The base path for all install prefixes '
+                 '(default: %(default)s)')
         parser.add_argument(
             '--merge-install',
             action='store_true',
